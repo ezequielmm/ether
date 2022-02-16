52d494e3cd98ca78bd1e7edf516bf27d89859074
ErikGurtovoi <erikpmcr@gmail.com>
2022-02-16 14:19:39 -0300

    +Corrected mistype on the GameManager script

--- KOTE_WebGL/Assets/Scripts/Managers/GameManager.cs
+++ KOTE_WebGL/Assets/Scripts/Managers/GameManager.cs
@@ -48,7 +48,7 @@ public class GameManager : SingleTon
     }
     public void LogOut()
     {
-        currentConnectionState = connectionState.;
+        currentConnectionState = connectionState.LoggedOut;
     }
 
 }
