5d6f93c955955da90dd779af3e6b75ad834fcc7c
ErikGurtovoi <erikpmcr@gmail.com>
2022-02-17 17:58:31 -0300

    ChangeLog -Removed EnumInvoker from this version

--- KOTE_WebGL/Assets/Scripts/UI/EnumInvoker.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class EnumInvoker : MonoBehaviour
-{
-    public inGameScenes Scene = default(inGameScenes);
-
-}
