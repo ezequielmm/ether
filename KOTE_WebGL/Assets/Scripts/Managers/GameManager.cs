17c7640a9b84c2999a656a10408bc1ed308c0f4a
ErikGurtovoi <erikpmcr@gmail.com>
2022-02-17 16:18:14 -0300

    Changelog +Removed every unrelatad file to the feature loading scene including -Game scene and map scene -Multiple scripts (BackButton, GeneralMenuButton, Login, main menu and registry files) -Prefabs(login and registry)

--- KOTE_WebGL/Assets/Scripts/Managers/GameManager.cs
+++ KOTE_WebGL/Assets/Scripts/Managers/GameManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public enum inGameScenes {MainMenu,Loader,Map,Game}; //created so we can use the names on the enums instead of hard coding strings everytime, if a scene name is changed we can just change it here as well instead of changing at various spots
+public enum inGameScenes {MainMenu,Loader}; //created so we can use the names on the enums instead of hard coding strings everytime, if a scene name is changed we can just change it here as well instead of changing at various spots
 
 public class GameManager : SingleTon<GameManager>
 {
