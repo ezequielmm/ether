f1256ceb549e3162fde2eb14f419783097c9aa7f
Danny López Cárdenas <70038382+DannyLopezC@users.noreply.github.com>
2022-03-06 13:37:14 -0500

    Logout confirmation panel

--- KOTE_WebGL/Assets/Scripts/Managers/SettingsManager.cs
+++ KOTE_WebGL/Assets/Scripts/Managers/SettingsManager.cs
@@ -8,15 +8,19 @@ public class SettingsManager : MonoBehaviour
 {
     public Slider volumeSlider;
     public GameObject settingsContainer;
+    public GameObject logoutConfirmContainer;
+    public Button logoutHyperlink;
 
     private void Start()
     {
         GameManager.Instance.EVENT_SETTINGSPANEL_ACTIVATION_REQUEST.AddListener(ActivateInnerSettingsPanel);
 
+        GameManager.Instance.EVENT_REQUEST_LOGIN_SUCESSFUL.AddListener(OnLogin);
         GameManager.Instance.EVENT_REQUEST_LOGOUT_ERROR.AddListener(OnLogoutError);
         GameManager.Instance.EVENT_REQUEST_LOGOUT_SUCCESSFUL.AddListener(OnLogoutSuccessful);
 
         volumeSlider.value = AudioListener.volume;
+        logoutHyperlink.interactable = false;
     }
 
     public void OnVolumeChanged()
@@ -35,8 +39,19 @@ public class SettingsManager : MonoBehaviour
         settingsContainer.SetActive(activate);
     }
 
+    public void ActivateInnerLogoutConfirmPanel(bool activate)
+    {
+        logoutConfirmContainer.SetActive(activate);
+    }
+
+    public void OnLogin(string name, int fief)
+    {
+        logoutHyperlink.interactable = true;
+    }
+
     public void OnLogout()
     {
+        logoutHyperlink.interactable = false;
         GameManager.Instance.EVENT_REQUEST_LOGOUT.Invoke(PlayerPrefs.GetString("session_token"));
     }
 
