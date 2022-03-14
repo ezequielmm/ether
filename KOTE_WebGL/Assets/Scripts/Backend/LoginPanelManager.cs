ebca85323e4437107fda59b10827613edba6282d
Danny López Cárdenas <70038382+DannyLopezC@users.noreply.github.com>
2022-03-14 17:41:59 -0500

    Remember me functionality

--- KOTE_WebGL/Assets/Scripts/Backend/LoginPanelManager.cs
+++ KOTE_WebGL/Assets/Scripts/Backend/LoginPanelManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
@@ -34,13 +35,31 @@ public class LoginPanelManager : MonoBehaviour
         GameManager.Instance.EVENT_REQUEST_LOGIN_SUCESSFUL.AddListener(OnLoginSucessful);
         GameManager.Instance.EVENT_REQUEST_LOGIN_ERROR.AddListener(OnLoginError);
         GameManager.Instance.EVENT_LOGINPANEL_ACTIVATION_REQUEST.AddListener(ActivateInnerLoginPanel);
+        GameManager.Instance.EVENT_REQUEST_LOGOUT_SUCCESSFUL.AddListener(OnLogoutSuccessful);
+
+        string email = PlayerPrefs.GetString("email_reme_login");
+        string date = PlayerPrefs.GetString("date_reme_login");
+
+        if (!string.IsNullOrEmpty(email) && !string.IsNullOrEmpty(date))
+        {
+            float days = (float) (DateTime.ParseExact(date, "MM/dd/yyyy HH:mm:ss",
+                CultureInfo.InvariantCulture) - DateTime.Today).TotalDays;
+
+            if (!(Mathf.Abs(days) >= 15))
+            {
+                emailInputField.text = email;
+                VerifyEmail();
+                rememberMe.isOn = true;
+            }
+        }
     }
 
     private void OnLoginSucessful(string userName, int fiefAmount)
     {
         if (rememberMe.isOn)
         {
-            PlayerPrefs.SetString("user_email", emailInputField.text);
+            PlayerPrefs.SetString("email_reme_login", emailInputField.text);
+            PlayerPrefs.SetString("date_reme_login", DateTime.Today.ToString(CultureInfo.InvariantCulture));
             PlayerPrefs.Save();
         }
 
@@ -51,6 +70,11 @@ public class LoginPanelManager : MonoBehaviour
     {
         validLoginPassword.gameObject.SetActive(true);
         passwordInputField.text = "";
+
+        PlayerPrefs.DeleteKey("email_reme_login");
+        PlayerPrefs.DeleteKey("date_reme_login");
+        PlayerPrefs.Save();
+
         Debug.Log("-------------------Login Error------------------");
     }
 
@@ -87,11 +111,13 @@ public class LoginPanelManager : MonoBehaviour
         }
     }
 
+    public void OnLogoutSuccessful(string token)
+    {
+        passwordInputField.text = "";
+    }
 
     public void ActivateInnerLoginPanel(bool activate)
     {
-        emailInputField.text = "";
-        passwordInputField.text = "";
         loginContainer.SetActive(activate);
     }
 }
