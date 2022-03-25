fd1b95ada6db3d9fdba8f1c416038b5e2f610c1d
Daniel del Castillo <ddelcastillo@robotseamonstergames.com>
2022-03-25 14:27:11 -0400

    fixing missing events

--- KOTE_WebGL/Assets/Scripts/Managers/GameManager.cs
+++ KOTE_WebGL/Assets/Scripts/Managers/GameManager.cs
@@ -62,7 +62,7 @@ public class GameManager : SingleTon<GameManager>
     [HideInInspector]
     public UnityEvent<bool> EVENT_WALLETSPANEL_ACTIVATION_REQUEST = new UnityEvent<bool>();
     [HideInInspector]
-    public UnityEvent<bool> EVENT_DISCONNECTWALLETPANEL_ACTIVATION_REQUEST = new UnityEvent<bool>();
+    public UnityEvent<bool,GameObject> EVENT_DISCONNECTWALLETPANEL_ACTIVATION_REQUEST = new UnityEvent<bool,GameObject>();
 
     //WALLETS EVENTS
     [HideInInspector]
@@ -83,12 +83,18 @@ public class GameManager : SingleTon<GameManager>
     //POTIONS EVENTS
     [HideInInspector]
     public UnityEvent<Potion> EVENT_POTION_USED = new UnityEvent<Potion>();
-    // ROYAL HOUSE EVENTS
+
+    //ROYAL HOUSE EVENTS
+    [HideInInspector]
+    public UnityEvent<bool> EVENT_ROYALHOUSES_ACTIVATION_REQUEST = new UnityEvent<bool>();
     [HideInInspector]
     public UnityEvent<ArmoryItem, bool> EVENT_SELECTARMORY_ITEM = new UnityEvent<ArmoryItem, bool>();
     [HideInInspector]
     public UnityEvent<BlessingItem, bool> EVENT_SELECTBLESSING_ITEM = new UnityEvent<BlessingItem, bool>();
-    
+
+    [HideInInspector]
+    public UnityEvent<bool> EVENT_SHOPLOCATION_ACTIVATION_REQUEST = new UnityEvent<bool>();
+
     public inGameScenes nextSceneToLoad; // maybe we can encapsulate this variable to control who can set it and allow all to get the value? Depending on the scene that is loaded there might be a change for a cheat
 
     public WebRequesterManager webRequester;
