2df7586c4f4591ae378dbc47a0e7685e4e11a69f
Andrew Koza <andrew.koza@kjastudios.com>
2022-05-24 09:47:34 -0700

    Updated the prefab to properly connect to the health slider. Added functionality to control the display of the acquired armor

--- KOTE_WebGL/Assets/Scripts/Combat/HealthBarManager.cs
+++ KOTE_WebGL/Assets/Scripts/Combat/HealthBarManager.cs
@@ -1,11 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
-using UnityEngine.UIElements;
+using UnityEngine.UI;
+
 
 public class HealthBarManager : MonoBehaviour
 {
-    public Slider HealthBar;
+    public Slider healthBar;
+    public GameObject armorGauge;
+    public TMP_Text armorText;
+
+    // keep track of the value locally so we can check when to hide it
+    private int blockAmount;
 
     // use this to outright set the amount of health the player has left
     private void SetCurrentHealth(int curHealth, int maxHealth)
@@ -13,21 +18,41 @@ public class HealthBarManager : MonoBehaviour
         float healthPercentage = (float)curHealth / maxHealth;
         if (healthPercentage < 0) healthPercentage = 0;
         if (healthPercentage > 1) healthPercentage = 1;
-        HealthBar.value = healthPercentage;
+        healthBar.value = healthPercentage;
     }
     
     // use these if the health bar itself is going to keep track of the amount of damage taken
     private void TakeDamage(int damage)
     {
         float damageTaken = damage / 10.0f;
-        HealthBar.value -= damageTaken;
-        if (HealthBar.value < 0) HealthBar.value = 0;
+        healthBar.value -= damageTaken;
+        if (healthBar.value < 0) healthBar.value = 0;
     }
 
     private void Heal(int healAmount)
     {
         float damageHealed = healAmount / 10.0f;
-        HealthBar.value += damageHealed;
-        if (HealthBar.value > 1) HealthBar.value = 1;
+        healthBar.value += damageHealed;
+        if (healthBar.value > 1) healthBar.value = 1;
+    }
+
+    // use these to add/remove block from the gauge, also handles showing the indicator
+    private void AddBlock(int blockAmount)
+    {
+        blockAmount += blockAmount;
+        armorText.text = blockAmount.ToString();
+        armorGauge.SetActive(true);
+    }
+    
+    private void RemoveBlock(int blockAmount)
+    {
+        blockAmount -= blockAmount;
+        if (blockAmount <= 0)
+        {
+            armorGauge.SetActive(false);
+            blockAmount = 0;
+        }
+        armorText.text = blockAmount.ToString();
+        
     }
 }
