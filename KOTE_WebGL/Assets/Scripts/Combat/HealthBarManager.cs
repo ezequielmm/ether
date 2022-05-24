a9f177afcc81305924d083be0910705e457eb237
Andrew Koza <andrew.koza@kjastudios.com>
2022-05-24 10:49:54 -0700

    Set up HealthBarManager to hook itself up to the relevant events for updating itself

--- KOTE_WebGL/Assets/Scripts/Combat/HealthBarManager.cs
+++ KOTE_WebGL/Assets/Scripts/Combat/HealthBarManager.cs
@@ -1,16 +1,38 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 
 public class HealthBarManager : MonoBehaviour
 {
+    [Tooltip("Set whether the health bar is attached to the player or an enemy")]
+    public EntityType combatEntityAttachedTo;
+    
+    //the various UI elements the manager controls
     public Slider healthBar;
-    public GameObject armorGauge;
-    public TMP_Text armorText;
+    public GameObject blockGauge;
+    public TMP_Text blockText;
 
     // keep track of the value locally so we can check when to hide it
     private int blockAmount;
+
+    private void Start()
+    {
+        if (combatEntityAttachedTo == EntityType.Player)
+        {
+            GameManager.Instance.EVENT_PLAYER_STATUS_UPDATE.AddListener(GetPlayerUpdate);
+        }
+        else if (combatEntityAttachedTo == EntityType.Enemy)
+        {
+            //TODO add a listener to whatever event tracks when the enemy status is updated
+        }
+    }
+
+    private void GetPlayerUpdate(PlayerStateData playerData)
+    {
+        SetCurrentHealth(playerData.data.player_state.hp_current, playerData.data.player_state.hp_max);
+    }
 
     // use this to outright set the amount of health the player has left
     private void SetCurrentHealth(int curHealth, int maxHealth)
@@ -20,39 +42,24 @@ public class HealthBarManager : MonoBehaviour
         if (healthPercentage > 1) healthPercentage = 1;
         healthBar.value = healthPercentage;
     }
-    
-    // use these if the health bar itself is going to keep track of the amount of damage taken
-    private void TakeDamage(int damage)
-    {
-        float damageTaken = damage / 10.0f;
-        healthBar.value -= damageTaken;
-        if (healthBar.value < 0) healthBar.value = 0;
-    }
 
-    private void Heal(int healAmount)
-    {
-        float damageHealed = healAmount / 10.0f;
-        healthBar.value += damageHealed;
-        if (healthBar.value > 1) healthBar.value = 1;
-    }
 
-    // use these to add/remove block from the gauge, also handles showing the indicator
-    private void AddBlock(int blockAmount)
+    private void SetCurrentBlock(int blockAmount)
     {
-        blockAmount += blockAmount;
-        armorText.text = blockAmount.ToString();
-        armorGauge.SetActive(true);
-    }
-    
-    private void RemoveBlock(int blockAmount)
-    {
-        blockAmount -= blockAmount;
-        if (blockAmount <= 0)
+        this.blockAmount = blockAmount;
+        if (this.blockAmount <= 0)
         {
-            armorGauge.SetActive(false);
-            blockAmount = 0;
+            this.blockAmount = 0;
+            blockText.text = this.blockAmount.ToString();
+            blockGauge.SetActive(false);
+            return;
         }
-        armorText.text = blockAmount.ToString();
-        
+        blockGauge.SetActive(true);
+    }
+
+    public enum EntityType
+    {
+        Player,
+        Enemy
     }
 }
