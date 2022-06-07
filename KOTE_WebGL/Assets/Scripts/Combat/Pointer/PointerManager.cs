26126843c34ab50df92eaef9f6769b642cb84b7a
Andrew Koza <andrew.koza@kjastudios.com>
2022-06-07 16:11:11 -0700

    Created a spine arrow/pointer to use as a placeholder. Added it to the prefab, and got it following the mouse when a card is dragged.

--- KOTE_WebGL/Assets/Scripts/Combat/Pointer/PointerManager.cs
+++ KOTE_WebGL/Assets/Scripts/Combat/Pointer/PointerManager.cs
@@ -1,3 +1,4 @@
+using Spine;
 using Spine.Unity;
 using UnityEngine;
 
@@ -5,24 +6,28 @@ public class PointerManager : MonoBehaviour
 {
     public GameObject pointerContainer;
     public GameObject pointerTarget;
-    public SkeletonAnimation SkeletonAnimation;
+    public SkeletonAnimation skeletonAnimation;
+    [SpineBone(dataField: "skeletonAnimation")]
+    public string boneName;
 
-    public bool overEnemy;
+    private Bone aimBone;
+    [HideInInspector] public bool overEnemy;
 
     private void Start()
     {
         GameManager.Instance.EVENT_CARD_ACTIVATE_POINTER.AddListener(OnPointerActivated);
         GameManager.Instance.EVENT_CARD_DEACTIVATE_POINTER.AddListener(OnPointerDeactivated);
+        aimBone = skeletonAnimation.Skeleton.FindBone(boneName);
     }
 
-   
 
     private void OnPointerActivated()
     {
         pointerContainer.SetActive(true);
         Vector3 position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         position.z = 0;
-        SkeletonAnimation.transform.position = position;
+        // I don't know why this is so weird, the x and y axis needs to be flipped, and there has to be an offset
+        aimBone.SetLocalPosition(skeletonAnimation.transform.InverseTransformPoint(new Vector3(position.y + 2, -position.x - 2, 0)));
         pointerTarget.transform.position = position;
     }
 
