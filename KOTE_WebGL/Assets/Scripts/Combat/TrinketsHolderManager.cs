356dce6ee10d244698ce543c5b9ac0d22ca06dd2
Andrew Koza <andrew.koza@kjastudios.com>
2022-06-09 15:01:37 -0700

    Created trinket holder prefab and trinket prefab. The trinket holder is notified when the player state is updated, and displays the provided trinkets based on the name of the trinket.

---
+++ KOTE_WebGL/Assets/Scripts/Combat/TrinketsHolderManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrinketsHolderManager : MonoBehaviour
+{
+    public GameObject trinketsContainer;
+
+    public TrinketItemManager trinketItem;
+
+    // we need this to set the parent of the created trinkets
+    public GameObject HorizontalGroup;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        // we might need to change this once we get trinkets properly implemented, idk
+        trinketsContainer.SetActive(false);
+        GameManager.Instance.EVENT_PLAYER_STATUS_UPDATE.AddListener(UpdateTrinketHolder);
+    }
+
+    // when we get a player status update, that tells us to update the trinkets
+    private void UpdateTrinketHolder(PlayerStateData playerState)
+    {
+        string[] activeTrinkets = playerState.data.player_state.trinkets;
+        // currently we're picking trinkets at random, that will change once we do more with them
+        if (activeTrinkets != null && activeTrinkets.Length > 0)
+        {
+            for (int i = 0; i < activeTrinkets.Length; i++)
+            {
+                TrinketItemManager trinket = Instantiate(trinketItem, HorizontalGroup.transform);
+                trinket.Populate("Trinket" + Random.Range(1, 5), "Common");
+            }
+            trinketsContainer.SetActive(true);
+        }
+    }
+}
