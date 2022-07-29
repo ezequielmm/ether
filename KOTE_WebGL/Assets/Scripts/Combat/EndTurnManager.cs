dd16545a02d0c4955a27698b4e53304968c6dcf2
sim2kid <sim2kid@users.noreply.github.com>
2022-07-29 16:09:16 -0500

    Update EndTurnManager.cs

--- KOTE_WebGL/Assets/Scripts/Combat/EndTurnManager.cs
+++ KOTE_WebGL/Assets/Scripts/Combat/EndTurnManager.cs
@@ -1,18 +1,66 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EndTurnManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    bool listenForEmptyQueue = false;
+    float timeLimit;
+    bool isPlayersTurn = true;
+    float originalPos;
+    RectTransform rt;
+
     void Start()
     {
-        
+        rt = GetComponent<RectTransform>();
+        GameManager.Instance.EVENT_CHANGE_TURN.AddListener(onTurnChange);
+        GameManager.Instance.EVENT_COMBAT_QUEUE_EMPTY.AddListener(onQueueEmpty);
+        originalPos = transform.position.x;
     }
 
-    // Update is called once per frame
+    void onQueueEmpty() {
+        if (listenForEmptyQueue) 
+        {
+            listenForEmptyQueue = false;
+            GameManager.Instance.EVENT_END_TURN_CLICKED.Invoke();
+        }
+    }
+
+    private void Update()
+    {
+        if (timeLimit > 0 && listenForEmptyQueue) 
+        {
+            timeLimit -= Time.deltaTime;
+            if (timeLimit <= 0) 
+            {
+                GameManager.Instance.EVENT_END_TURN_CLICKED.Invoke();
+            }
+        }
+    }
+
+    void onTurnChange(string who) 
+    {
+        if (who == "player")
+        {
+            transform.DOMoveX(originalPos, 0.5f);
+            listenForEmptyQueue = false;
+            isPlayersTurn = true;
+        }
+        else if(who == "enemy")
+        {
+            transform.DOMoveX(originalPos + rt.rect.width + 5, 0.5f);
+            listenForEmptyQueue = true;
+            timeLimit += 10; // This is only because sometimes the enemy does nothing
+            isPlayersTurn = false;
+        }
+    }
+
     public void EndTurn()
     {
-        GameManager.Instance.EVENT_END_TURN_CLICKED.Invoke();
+        if (isPlayersTurn)
+        {
+            GameManager.Instance.EVENT_END_TURN_CLICKED.Invoke();
+        }
     }
 }
