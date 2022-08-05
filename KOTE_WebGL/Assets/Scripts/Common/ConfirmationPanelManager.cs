ed5f188b94fb7eb54d4ec1155ce095b0b076cb0f
Andrew Koza <andrew.koza@kjastudios.com>
2022-08-05 12:11:30 -0700

    modified confirmationPanel to have more control options.
    Also added a no wallet connected question panel on play button without wallet attached.

--- KOTE_WebGL/Assets/Scripts/Common/ConfirmationPanelManager.cs
+++ KOTE_WebGL/Assets/Scripts/Common/ConfirmationPanelManager.cs
@@ -13,35 +13,73 @@ public class ConfirmationPanelManager : MonoBehaviour
     public GameObject confirmationPanelContainer;
     [Tooltip("The title text for the confirmation panel")]
     public TMP_Text titleText;
-    
+    [Tooltip("The text for the confirmation button")]
+    public TMP_Text confirmText;
+    [Tooltip("The text for the cancel button")]
+    public TMP_Text cancelText;
 
     // store the action so we can delete it after the player makes a selection
-    private Action currentOnClickAction;
+    private Action currentOnConfirmAction;
+    private Action currentOnCancelFunction;
     private void Start()
     {
         confirmationPanelContainer.SetActive(false);
-        GameManager.Instance.EVENT_SHOW_CONFIRMATION_PANEL.AddListener(ShowNodeSelectionRequest);
+        GameManager.Instance.EVENT_SHOW_CONFIRMATION_PANEL.AddListener(ShowConfirmationPanel);
+        GameManager.Instance.EVENT_SHOW_CONFIRMATION_PANEL_WITH_BACK_ACTION.AddListener(ShowConfirmationPanelWithBackAction);
+        GameManager.Instance.EVENT_SHOW_CONFIRMATION_PANEL_WITH_FULL_CONTROL.AddListener(ShowConfirmationPanelWIthFullControl);
 
     }
 
-    private void ShowNodeSelectionRequest(string displayText, Action onClickFunction)
+    private void ShowConfirmationPanel(string displayText, Action onConfirmFunction)
     {
         titleText.text = displayText;
         // store and add the onClickFunction for the button to listen to.
-        currentOnClickAction = onClickFunction;
+        currentOnConfirmAction = onConfirmFunction;
+        confirmText.text = "Confirm";
+        cancelText.text = "Back";
        
+        confirmationPanelContainer.SetActive(true);
+    }
+    
+    //override to allow the back button to do more than hide the confirmation window
+    private void ShowConfirmationPanelWithBackAction(string displayText, Action onConfirmFunction, Action onCancelFunction)
+    {
+        titleText.text = displayText;
+        // store and add the onClickFunction for the button to listen to.
+        currentOnConfirmAction = onConfirmFunction;
+        currentOnCancelFunction = onCancelFunction;
+        confirmText.text = "Confirm";
+        cancelText.text = "Back";
+        
+        confirmationPanelContainer.SetActive(true);
+    }
+
+    private void ShowConfirmationPanelWIthFullControl(string displayText, Action onConfirmFunction,
+        Action onCancelFunction, string[] buttonTexts)
+    {
+        titleText.text = displayText;
+        // store and add the onClickFunction for the button to listen to.
+        currentOnConfirmAction = onConfirmFunction;
+        currentOnCancelFunction = onCancelFunction;
+
+        confirmText.text = buttonTexts[0];
+        cancelText.text = buttonTexts[1];
+        
         confirmationPanelContainer.SetActive(true);
     }
 
     public void OnCancel()
     {
+        if (currentOnCancelFunction != null) currentOnCancelFunction();
+        currentOnCancelFunction = null;
         // deactivate the panel and get rid of the action that was sent
         confirmationPanelContainer.SetActive(false);   
     }
 
     public void OnConfirmation()
     {
-        currentOnClickAction();
+        // don't need to null check this as it will always receive a confirmation action
+        currentOnConfirmAction();
         confirmationPanelContainer.SetActive(false);
     }
 }
