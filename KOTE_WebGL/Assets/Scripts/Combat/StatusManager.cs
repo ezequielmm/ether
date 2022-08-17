2d066b1d3ad012a91ae98991a06ce4cf23cbd1a7
sim2kid <sim2kid@users.noreply.github.com>
2022-08-17 13:14:08 -0500

    Icon spacer now can handle modifying icons

--- KOTE_WebGL/Assets/Scripts/Combat/StatusManager.cs
+++ KOTE_WebGL/Assets/Scripts/Combat/StatusManager.cs
@@ -17,6 +17,7 @@ public class StatusManager : MonoBehaviour
     PlayerManager playerManager;
 
     List<Status> statusList = new List<Status>();
+    Dictionary<string, GameObject> statusIconList = new Dictionary<string, GameObject>();
 
     string entityType => (enemyManager == null ? "player" : "enemy");
     int entityID => enemyManager?.EnemyData.enemyId ?? playerManager?.PlayerData.playerId ?? -1;
@@ -61,34 +62,57 @@ public class StatusManager : MonoBehaviour
         Utils.GizmoDrawBox(new Bounds(rt.position + transform.position, size2 * 1.05f), new Vector3(size.x * 2, 0, transform.position.z - 0.1f));
     }
 
-    //private void onTurnChange(string next) 
-    //{
-    //    if (!askedForStatus)
-    //    {
-    //        askForStatus();
-    //    }
-    //}
-
-    private void DrawStatus() 
+    private GameObject createIcon(Status status) 
     {
-        iconContainer.ClearIcons();
-        foreach (var status in statusList) 
+        GameObject iconObject = Instantiate(iconPrefab);
+        var statusIcon = iconObject.GetComponent<StatusIcon>();
+        statusIcon.Initialize();
+        setStatusInfo(status, statusIcon);
+        return iconObject;
+    }
+
+    private void setStatusInfo(Status status, StatusIcon statusIcon)
+    {
+        STATUS stat = ToEnum(status.name);
+        statusIcon.SetValue(status.counter);
+        statusIcon.SetIcon(stat);
+        statusIcon.SetTooltip(status.description);
+    }
+
+    private void setStatusInfo(Status status, GameObject iconObject) 
+    {
+        var statusIcon = iconObject.GetComponent<StatusIcon>();
+        setStatusInfo(status, statusIcon);
+    }
+
+    private void DrawStatus(List<Status> newStatus) 
+    {
+        foreach (var status in newStatus) 
         {
-            GameObject iconObject = Instantiate(iconPrefab);
-            var statusIcon = iconObject.GetComponent<StatusIcon>();
-            statusIcon.Initialize();
-
-            STATUS stat = ToEnum(status.name);
-            statusIcon.SetValue(status.counter);
-            statusIcon.SetIcon(stat);
-            statusIcon.SetTooltip(status.description);
-
-            iconContainer.AddIcon(iconObject);
+            var hasKey = statusIconList.ContainsKey(status.name);
+            if (!hasKey) // New not in known
+            {
+                var newIcon = createIcon(status);
+                statusIconList.Add(status.name, newIcon);
+                iconContainer.AddIcon(newIcon);
+            }
+            else // New and Known Match
+            {
+                var icon = statusIconList[status.name];
+                setStatusInfo(status, icon);
+            }
         }
+        List<string> keysToDelete = new List<string>();
+        foreach (string key in statusIconList.Keys) 
+        {
+            if (newStatus.Find(status => status.name == key) == null) // Known not in New
+            {
+                iconContainer.DeleteIcon(statusIconList[key]);
+                keysToDelete.Add(key);
+            }
+        }
+        foreach (string key in keysToDelete) { statusIconList.Remove(key); }
         iconContainer.ReorganizeSprites();
-
-        askedForStatus = false;
-        statusSet = true;
     }
 
     private void Update()
@@ -109,24 +133,24 @@ public class StatusManager : MonoBehaviour
 
     public void UpdateStatus(List<Status> newStatuses) 
     {
-        foreach (var newStatus in newStatuses)
+        foreach (var status in newStatuses)
         {
             bool set = false;
             for (int i = 0; i < statusList.Count; i++)
             {
-                if (statusList[i].name == newStatus.name)
+                if (statusList[i].name == status.name)
                 {
-                    statusList[i] = newStatus;
+                    statusList[i] = status;
                     set = true;
                     break;
                 }
             }
             if (!set)
             {
-                statusList.Add(newStatus);
+                statusList.Add(status);
             }
         }
-        DrawStatus();
+        DrawStatus(statusList);
     }
 
     private void OnSetStatus(StatusData status) 
@@ -134,7 +158,7 @@ public class StatusManager : MonoBehaviour
         if ((status.targetEntity != entityType || status.id != entityID) && (status.targetEntity != "all" || status.targetEntity != "player")) return;
 
         statusList = status.statuses;
-        DrawStatus();
+        DrawStatus(status.statuses);
     }
 
     private STATUS ToEnum(string str) 
