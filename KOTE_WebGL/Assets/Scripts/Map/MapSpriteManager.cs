d10009136c7aac92137b9e6665bec16d55812dcf
Andrew Koza <andrew.koza@kjastudios.com>
2022-08-31 16:14:06 -0700

    hooked up the map knight icon to move when a node is selected

--- KOTE_WebGL/Assets/Scripts/Map/MapSpriteManager.cs
+++ KOTE_WebGL/Assets/Scripts/Map/MapSpriteManager.cs
@@ -65,13 +65,14 @@ namespace map
             GameManager.Instance.EVENT_MAP_MASK_DOUBLECLICK.AddListener(OnMaskDoubleClick);
             GameManager.Instance.EVENT_MAP_ACTIVATE_PORTAL.AddListener(OnPortalActivated);
             GameManager.Instance.EVENT_MAP_REVEAL.AddListener(OnRevealMap);
+            GameManager.Instance.EVENT_MAP_NODE_SELECTED.AddListener(OnNodeSelected);
 
             playerIcon.SetActive(false);
 
             // we need half the width of the screen for various checks
             halfScreenWidth = Camera.main.orthographicSize * Camera.main.aspect;
             maskBounds = GetComponentInChildren<SpriteMask>().GetComponent<BoxCollider2D>().bounds;
-            
+
             // the particleSystem's sorting layer has to be set manually, because the the settings in the component don't work
             portalAnimation.GetComponent<Renderer>().sortingLayerName = GameSettings.MAP_ELEMENTS_SORTING_LAYER_NAME;
         }
@@ -218,6 +219,15 @@ namespace map
             }
         }
 
+        private void OnNodeSelected(int id)
+        {
+            if (nodes.Exists(node => node.id == id))
+            {
+                NodeData curNode = nodes.Find(node => node.id == id);
+                playerIcon.transform.localPosition = curNode.transform.localPosition;
+            }
+        }
+
         private void OnMapNodesDataUpdated(SWSM_MapData mapData)
         {
             GenerateMap(mapData);
@@ -273,7 +283,7 @@ namespace map
                 nodes.Remove(node);
                 Destroy(node.gameObject);
             }
-            
+
             // clear the references to the map paths
             pathManagers.Clear();
         }
@@ -457,32 +467,32 @@ namespace map
                         Vector3 pointPosition = pathSpline.GetPosition(i);
                         pointPosition = pathTransform.TransformPoint(pointPosition);
                         Vector3Int cellPosition = MapGrid.layoutGrid.WorldToCell(pointPosition);
-                        
+
                         // we have to set the z to a constant, as for some reason you can two tiles in the same spot with different z levels
                         cellPosition = new Vector3Int(cellPosition.x, cellPosition.y,
                             (int)GameSettings.MAP_SPRITE_ELEMENTS_Z);
 
                         int randomTile = Random.Range(0, grassTiles.Length);
                         MapGrid.SetTile(cellPosition, grassTiles[randomTile]);
-                        
+
                         // if it's not the last point on the spline, move towards the next point and mark the tiles as path
                         if (i != splinePoints - 1)
                         {
                             Vector3 nextPoint = pathTransform.TransformPoint(pathSpline.GetPosition(i + 1));
                             Vector3 nextTilePos = Vector3.MoveTowards(pointPosition, nextPoint, 0.1f);
-                            
+
                             while (Vector3.Distance(nextTilePos, nextPoint) > 0.1f)
                             {
                                 Vector3Int nextTilePosition = MapGrid.layoutGrid.WorldToCell(nextTilePos);
-                                
+
                                 // we have to set the z to a constant, as for some reason you can two tiles in the same spot with different z levels
                                 nextTilePosition = new Vector3Int(nextTilePosition.x, nextTilePosition.y,
                                     (int)GameSettings.MAP_SPRITE_ELEMENTS_Z);
-                                
+
                                 // set a random grass tile at that position
                                 randomTile = Random.Range(0, grassTiles.Length);
                                 MapGrid.SetTile(nextTilePosition, grassTiles[randomTile]);
-                                
+
                                 nextTilePos = Vector3.MoveTowards(nextTilePos, nextPoint, 0.1f);
                             }
                         }
