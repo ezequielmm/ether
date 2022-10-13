b8712c784b1ad50c82b273599307c2a700e975da
Andrew Koza <andrew.koza@kjastudios.com>
2022-10-13 11:21:33 -0700

    Finished implementing the card select logic

--- KOTE_WebGL/Assets/Scripts/Common/CardPanel/SelectCardsPanel.cs
+++ KOTE_WebGL/Assets/Scripts/Common/CardPanel/SelectCardsPanel.cs
@@ -10,8 +10,8 @@ public class SelectCardsPanel : CardPanelBase
     public GameObject selectCardPrefab;
     public Button selectButton;
     private int cardsToSelect;
-    private int selectedCards;
-    private List<string> selectedCardIds = new List<string>();
+    [SerializeField] [ReadOnly] private int selectedCards;
+    [SerializeField] [ReadOnly] private List<string> selectedCardIds = new List<string>();
 
     // Start is called before the first frame update
     protected override void Start()
@@ -19,7 +19,7 @@ public class SelectCardsPanel : CardPanelBase
         base.Start();
         GameManager.Instance.EVENT_SHOW_SELECT_CARD_PANEL.AddListener(OnShowSelectCardPanel);
     }
-    
+
     private void OnShowSelectCardPanel(List<Card> selectableCards, int numberToSelect,
         Action<List<string>> onFinishedSelection)
     {
@@ -30,29 +30,33 @@ public class SelectCardsPanel : CardPanelBase
             GameObject newCard = Instantiate(selectCardPrefab, gridCardsContainer.transform);
             SelectableUiCardManager cardManager = newCard.GetComponent<SelectableUiCardManager>();
             cardManager.Populate(card);
-            
+
             // set the toggle behavior to keep track what cards are selected
             cardManager.cardSelectorToggle.onValueChanged.AddListener((isOn) =>
             {
-                if (isOn)
+                if (cardManager.isSelected == false && selectedCards != cardsToSelect)
                 {
                     selectedCards++;
                     selectedCardIds.Add(cardManager.GetId());
-                    
+                    cardManager.isSelected = true;
                 }
-                else
+                else if (cardManager.isSelected)
                 {
                     selectedCards--;
                     selectedCardIds.Remove(cardManager.GetId());
+                    cardManager.isSelected = false;
                 }
 
-                if (selectedCards == cardsToSelect) selectButton.interactable = true;
-                else selectButton.interactable = false;
+                if (selectedCards == cardsToSelect) selectButton.gameObject.SetActive(true);
+                else selectButton.gameObject.SetActive(false);
+
+                cardManager.DetermineToggleColor();
             });
         }
 
         cardsToSelect = numberToSelect;
         selectButton.onClick.AddListener(() => { onFinishedSelection(selectedCardIds); });
+        selectButton.gameObject.SetActive(false);
         commonCardsContainer.SetActive(true);
     }
 }
