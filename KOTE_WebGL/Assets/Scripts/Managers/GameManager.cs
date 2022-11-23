11f52457da65541c1740284123e8878757364f0a
Andrew Koza <andrew.koza@kjastudios.com>
2022-11-22 16:14:11 -0800

    Finished setting up treasure panel to deal with damage traps

--- KOTE_WebGL/Assets/Scripts/Managers/GameManager.cs
+++ KOTE_WebGL/Assets/Scripts/Managers/GameManager.cs
@@ -111,6 +111,7 @@ public class GameManager : SingleTon<GameManager>
     [HideInInspector] public UnityEvent<SWSM_TreasureData> EVENT_TREASURE_CHEST_SIZE = new UnityEvent<SWSM_TreasureData>();
     [HideInInspector] public UnityEvent EVENT_TREASURE_OPEN_CHEST = new UnityEvent();
     [HideInInspector] public UnityEvent<SWSM_ChestResult> EVENT_TREASURE_CHEST_RESULT = new UnityEvent<SWSM_ChestResult>();
+    [HideInInspector] public UnityEvent<int> EVENT_ENCOUNTER_DAMAGE = new UnityEvent<int>();
 
 
     //UPGRADE EVENTS
