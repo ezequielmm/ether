dfffe89fa7c7a88f362a46b9b866ae9d383a0933
Andrew Koza <andrew.koza@kjastudios.com>
2022-12-06 09:28:28 -0800

    more updates to encounter setup

--- KOTE_WebGL/Assets/Scripts/Managers/EncounterManager.cs
+++ KOTE_WebGL/Assets/Scripts/Managers/EncounterManager.cs
@@ -11,11 +11,13 @@ public class EncounterManager : MonoBehaviour
     private void Start()
     {
         GameManager.Instance.EVENT_SHOW_ENCOUNTER_PANEL.AddListener(ShowEncounterPanel);
+        GameManager.Instance.EVENT_POPULATE_ENCOUNTER_PANEL.AddListener(OnPopulateEncounter);
     }
     
     public void ShowEncounterPanel()
     {
         EncounterContainer.SetActive(true);
+        GameManager.Instance.EVENT_GENERIC_WS_DATA.Invoke(WS_DATA_REQUEST_TYPES.EncounterData);
     }
     public void OnOptionOne()
     {
@@ -33,6 +35,11 @@ public class EncounterManager : MonoBehaviour
         GameManager.Instance.EVENT_ENCOUNTER_OPTION_SELECTED.Invoke(2);
         GameManager.Instance.EVENT_PLAY_SFX.Invoke(SoundTypes.UI, "Button Click");
         GameManager.Instance.EVENT_CONTINUE_EXPEDITION.Invoke();
+    }
+
+    private void OnPopulateEncounter(SWSM_EncounterData encounterData)
+    {
+        
     }
 
     private void PopulateEncounterText(string text)
