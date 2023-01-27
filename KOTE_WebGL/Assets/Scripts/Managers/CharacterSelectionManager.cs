3124fb0f683cb54963af5b8518290bdaccd4bc0d
Andrew Koza <andrew.koza@kjastudios.com>
2023-01-27 14:33:39 -0800

    clear nfts on metadata request

--- KOTE_WebGL/Assets/Scripts/Managers/CharacterSelectionManager.cs
+++ KOTE_WebGL/Assets/Scripts/Managers/CharacterSelectionManager.cs
@@ -19,7 +19,7 @@ public class CharacterSelectionManager : MonoBehaviour
     {
         //GameManager.Instance.webRequester.RequestCharacterList();// we are not requesting the list until we have more than one type so for the moment only knight
 
-        GameManager.Instance.EVENT_WALLET_ADDRESS_RECEIVED.AddListener(OnWalletReceived);
+        GameManager.Instance.EVENT_REQUEST_NFT_METADATA.AddListener(OnMetadataRequest);
         GameManager.Instance.EVENT_CHARACTERSELECTIONPANEL_ACTIVATION_REQUEST.AddListener(
             ActivateInnerCharacterSelectionPanel);
         GameManager.Instance.EVENT_EXPEDITION_CONFIRMED.AddListener(OnExpeditionConfirmed);
@@ -34,7 +34,7 @@ public class CharacterSelectionManager : MonoBehaviour
     }
 
     // when a new wallet is received clear the panel
-    private void OnWalletReceived(string address)
+    private void OnMetadataRequest(int[] ids)
     {
         for (int i = 0; i < nftSelectionLayout.transform.childCount; i++)
         {
--- KOTE_WebGL/Assets/Scripts/Treasury/TreasuryManager.cs
+++ KOTE_WebGL/Assets/Scripts/Treasury/TreasuryManager.cs
@@ -27,7 +27,7 @@ public class TreasuryManager : MonoBehaviour
 
         GameManager.Instance.EVENT_TREASURYPANEL_ACTIVATION_REQUEST.AddListener(ActivateInnerTreasuryPanel);
         GameManager.Instance.EVENT_NFT_METADATA_RECEIVED.AddListener(SetNftPanelContent);
-        GameManager.Instance.EVENT_WALLET_ADDRESS_RECEIVED.AddListener(OnWalletReceived);
+        GameManager.Instance.EVENT_REQUEST_NFT_METADATA.AddListener(OnMetadataRequested);
         Button firstCharacterButton = characterList.transform.GetChild(0)?.GetComponent<Button>();
         if (firstCharacterButton != null) firstCharacterButton.onClick?.Invoke();
     }
@@ -42,7 +42,7 @@ public class TreasuryManager : MonoBehaviour
         SetArmorPanelContent();
     }
     // when a new wallet is received clear the panel
-    private void OnWalletReceived(string address)
+    private void OnMetadataRequested(int[] ids)
     {
         for (int i = 0; i < nftPanel.transform.childCount; i++)
         {
