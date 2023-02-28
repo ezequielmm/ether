e59f19c5aaeb7a4e058688c0e67b02a7b402e77b
Owen Ravelo <sim2kid@users.noreply.github.com>
2023-02-28 15:33:02 -0600

    Remove random Using

--- KOTE_WebGL/Assets/Scripts/Managers/WebSocketManager.cs
+++ KOTE_WebGL/Assets/Scripts/Managers/WebSocketManager.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using BestHTTP.SocketIO3;
 using BestHTTP.SocketIO3.Events;
-using CodiceApp.EventTracking.Plastic;
 using Cysharp.Threading.Tasks;
 using Newtonsoft.Json;
 using UnityEngine;
