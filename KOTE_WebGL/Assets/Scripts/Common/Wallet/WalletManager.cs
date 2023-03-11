4943998e5174b4b5c6d5d6689cba74cedae60a14
Andrew Koza <53100998+KJAstudios@users.noreply.github.com>
2023-03-10 19:09:21 -0800

    removed bad import

--- KOTE_WebGL/Assets/Scripts/Common/Wallet/WalletManager.cs
+++ KOTE_WebGL/Assets/Scripts/Common/Wallet/WalletManager.cs
@@ -1,24 +1,26 @@
-using Cysharp.Threading.Tasks;
 using System;
 using System.Collections.Generic;
-using Unity.Plastic.Newtonsoft.Json;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class WalletManager : ISingleton<WalletManager>
 {
     private static WalletManager instance;
-    public static WalletManager Instance 
+
+    public static WalletManager Instance
     {
-        get 
+        get
         {
-            if(instance == null) 
+            if (instance == null)
             {
                 instance = new WalletManager();
             }
+
             return instance;
         }
     }
+
     public void DestroyInstance()
     {
         instance = null;
@@ -34,31 +36,33 @@ public class WalletManager : ISingleton<WalletManager>
 
     private MetaMask metaMask;
 
-    private WalletManager() 
+    private WalletManager()
     {
         metaMask = MetaMask.Instance;
     }
 
-    public async void SetActiveWallet() 
+    public async void SetActiveWallet()
     {
         await ConnectWallet();
         WalletStatusModified.Invoke();
         bool ownWallet = await ConfirmActiveWalletOwnership();
         WalletStatusModified.Invoke();
-        if (!ownWallet) 
+        if (!ownWallet)
         {
             return;
         }
+
         List<int> knights = await GetNftsInWalletPerContract(NftContract.KnightsOfTheEther);
         WalletStatusModified.Invoke();
-        if (knights == null) 
+        if (knights == null)
         {
             // Could not get knights
             GameManager.Instance.EVENT_SHOW_CONFIRMATION_PANEL.Invoke(
                 "ERROR: Could not gather wallet contents. Please try again later.", () => { });
             return;
         }
-        if(knights.Count <= 0) 
+
+        if (knights.Count <= 0)
         {
             // No Knights
             GameManager.Instance.EVENT_SHOW_CONFIRMATION_PANEL.Invoke(
@@ -71,23 +75,24 @@ public class WalletManager : ISingleton<WalletManager>
         WalletStatusModified.Invoke();
     }
 
-    public async UniTask ConnectWallet() 
+    public async UniTask ConnectWallet()
     {
 #if UNITY_EDITOR
         SetWallet(GameSettings.EDITOR_WALLET);
         return;
 #endif
         string activeAccount = await metaMask.RequestAccount();
-        if(activeAccount == null) 
+        if (activeAccount == null)
         {
             return;
         }
+
         SetWallet(activeAccount);
     }
 
-    public async UniTask<bool> ConfirmActiveWalletOwnership() 
+    public async UniTask<bool> ConfirmActiveWalletOwnership()
     {
-        if(string.IsNullOrEmpty(ActiveWallet)) 
+        if (string.IsNullOrEmpty(ActiveWallet))
         {
             Debug.LogWarning($"[NftManager] Can not sign message without a wallet.");
             return false;
@@ -98,61 +103,70 @@ public class WalletManager : ISingleton<WalletManager>
 #endif
         // TODO: Check backend if wallet was previously authorized and is still valid.
 
-        var message = $"Hello, welcome to Knights of the Ether.\nPlease sign this message to verify your wallet.\nThis action will not cost you any transaction fee.\n\n\nSecret Code: {Guid.NewGuid()}";
+        var message =
+            $"Hello, welcome to Knights of the Ether.\nPlease sign this message to verify your wallet.\nThis action will not cost you any transaction fee.\n\n\nSecret Code: {Guid.NewGuid()}";
         WalletSignature walletSignature = new WalletSignature(ActiveWallet, message);
         Debug.Log($"[NftManager] Signing Message:\n{message}");
         bool signSuccessful = await walletSignature.SignWallet();
-        if(!signSuccessful) 
+        if (!signSuccessful)
         {
             Debug.LogWarning($"[NftManager] Could not get wallet signature. Wallet not verified.");
             return false;
         }
-        if (!await FetchData.Instance.VerifyWallet(walletSignature)) 
+
+        if (!await FetchData.Instance.VerifyWallet(walletSignature))
         {
             Debug.Log($"[NftManager] Wallet was not verified by backend.");
             return false;
         }
+
         Debug.LogWarning($"[NftManager] Wallet signature verified!");
         return true;
     }
 
-    public void SelectedAccountChanged(string newAccount) 
+    public void SelectedAccountChanged(string newAccount)
     {
         SetWallet(newAccount);
     }
 
-    public void ForgetWallet(string walletToRemove) 
+    public void ForgetWallet(string walletToRemove)
     {
-        if(walletToRemove == ActiveWallet) { RemoveActiveWallet(); }
+        if (walletToRemove == ActiveWallet)
+        {
+            RemoveActiveWallet();
+        }
     }
 
-    public void RemoveActiveWallet() 
+    public void RemoveActiveWallet()
     {
         DisconnectingWallet.Invoke(ActiveWallet);
         SetWallet(null);
     }
 
-    private void SetWallet(string newAddress) 
+    private void SetWallet(string newAddress)
     {
         WalletVerified = false;
         ActiveWallet = newAddress;
         NftsInWallet.Clear();
     }
 
-    public async UniTask<List<int>> GetNftsInWalletPerContract(NftContract contract) 
+    public async UniTask<List<int>> GetNftsInWalletPerContract(NftContract contract)
     {
         return await GetNftsInWalletPerContract(contract, ActiveWallet);
     }
+
     public async UniTask<List<int>> GetNftsInWalletPerContract(NftContract contract, string walletAddress)
     {
         if (contract != NftContract.KnightsOfTheEther)
         {
             throw new NotImplementedException();
         }
+
         if (NftsInWallet.ContainsKey(contract))
         {
             return NftsInWallet[contract];
         }
+
         string contractAddress = GetNftContractAddress(contract);
         Debug.Log($"[WalletManager] Fetching Wallet Contents...");
         WalletData nftData = await FetchData.Instance.GetNftsInWalletPerContract(walletAddress, contractAddress);
@@ -180,7 +194,8 @@ public class WalletManager : ISingleton<WalletManager>
         if (nfts == null) return 0;
         return nfts.Count;
     }
-    private static string GetNftContractAddress(NftContract contract) 
+
+    private static string GetNftContractAddress(NftContract contract)
     {
         return NftManager.GetNftContractAddress(contract);
     }
