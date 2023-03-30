3910679f091c8fc54c7a040963b6bf3fa16f859e
Andrew Koza <53100998+KJAstudios@users.noreply.github.com>
2023-03-29 17:49:35 -0700

    fixed an issue with the wrong skin showing

--- KOTE_WebGL/Assets/Scripts/UI/Armory/ArmoryPanelManager.cs
+++ KOTE_WebGL/Assets/Scripts/UI/Armory/ArmoryPanelManager.cs
@@ -52,6 +52,7 @@ namespace KOTE.UI.Armory
         private void ActivateContainer(bool show)
         {
             // run this whe the panel is opened, instead of when nfts load, so images are cached
+            GameManager.Instance.EVENT_NFT_SELECTED.Invoke(curNode.Value.MetaData);
             UpdatePanelOnNftUpdate();
             panelContainer.SetActive(show);
         }
