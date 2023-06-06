16eed655d7da23b2ea43a8958ad7bb8669d6beb4
Marcelo Cotrofe <marcelo.cotrofe@gmail.com>
2023-06-06 12:54:29 -0300

    main menu scene and images

--- KOTE_WebGL/Assets/Scripts/Managers/LoadingManager.cs
+++ KOTE_WebGL/Assets/Scripts/Managers/LoadingManager.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using TMPro;
 using DefaultNamespace;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.ResourceManagement.ResourceProviders;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
@@ -39,10 +42,7 @@ public class LoadingManager : MonoBehaviour
     }
     public void Login(string loginData)
     {
-
-
         AuthenticationManager.Token = loginData;
-
         Debug.Log("Init login with " + loginData);
         loader.Show();
         if (GameManager.Instance.firstLoad)
@@ -51,11 +51,9 @@ public class LoadingManager : MonoBehaviour
             LoadWithEnvironmentCheck();
             return;
         }
-
         Debug.Log("Login not first load ::  " + GameManager.Instance.nextSceneToLoad.ToString());
-        StartCoroutine(LoadAsynchronously(GameManager.Instance.nextSceneToLoad.ToString()));
+        LoadMainMenuScene();
         DontDestroyOnLoad(gameObject);
-
         if (LoadGroup)
             LoadGroup.SetActive(true);
     }
@@ -63,14 +61,20 @@ public class LoadingManager : MonoBehaviour
     private async void LoadWithEnvironmentCheck()
     {
         await ClientEnvironmentManager.Instance.StartEnvironmentManger();
-        StartCoroutine(LoadAsynchronously(GameManager.Instance.nextSceneToLoad.ToString()));
+        LoadMainMenuScene();
         GameManager.Instance.firstLoad = false;
     }
+
+    private void LoadMainMenuScene()
+    {
+        //StartCoroutine(LoadAsynchronously(GameManager.Instance.nextSceneToLoad.ToString()));
+        StartCoroutine(LoadAsynchronouslyFromAddressables(GameManager.Instance.nextSceneToLoad.ToString()));
+    }
+    
 
     IEnumerator LoadAsynchronously(string sceneName)
     {
         // The Application loads the Scene in the background as the current Scene runs.
-
         AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName);
         Debug.Log(" LoadAsynchronously " + sceneName);
         // Wait until the asynchronous scene fully loads
@@ -81,11 +85,25 @@ public class LoadingManager : MonoBehaviour
                 loadingText.text = "Loading...\n(" + 100 * asyncLoad.progress + "%)"; //shows percentage
                 slideBar.value = asyncLoad.progress; //charges the load bar
             }
-
             //Debug.Log(asyncLoad.progress);
             yield return null;
         }
-
+        // Game manager is now listening for the sceneLoaded event from SceneManager, instead of calling it directly
+        Destroy(gameObject);
+    }
+    
+    IEnumerator LoadAsynchronouslyFromAddressables(string sceneName)
+    {
+        var asyncLoad = Addressables.LoadSceneAsync("Assets/Scenes/MainMenu.unity");
+        while (!asyncLoad.IsDone)
+        {
+            if (loadingText != null && slideBar != null)
+            {
+                loadingText.text = "Loading...\n(" + 100 * asyncLoad.PercentComplete + "%)"; //shows percentage
+                slideBar.value = asyncLoad.PercentComplete; //charges the load bar
+            }
+            yield return null;
+        }
         // Game manager is now listening for the sceneLoaded event from SceneManager, instead of calling it directly
         Destroy(gameObject);
     }
