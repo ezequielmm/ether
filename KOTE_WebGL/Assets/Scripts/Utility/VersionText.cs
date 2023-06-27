88903df6ed3711197f911467b4ad0cde11efdea9
DreadZito <hernanmontenegro011023@gmail.com>
2023-06-27 18:33:44 -0300

    version text

---
+++ KOTE_WebGL/Assets/Scripts/Utility/VersionText.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+namespace DefaultNamespace.Utility
+{
+    public class VersionText : MonoBehaviour
+    {
+        private void Start()
+        {
+            if (TryGetComponent<TextMeshProUGUI>(out var text))
+            {
+                text.text = Application.version;
+            }
+        }
+    }
+}
